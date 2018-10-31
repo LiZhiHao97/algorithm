7372311e20f5567e5995419b971df3fd81d4d041
lizhihao97 <940166841@qq.com>
2018-10-31 22:30:59 +0800

    fix 002

--- leetcode/002_Add_Two_Numbers/solution.js
+++ leetcode/002_Add_Two_Numbers/solution.js
@@ -14,11 +14,11 @@ function addTwoNumbers(l1, l2) {
     const resLinkedList = []
     let remain = 0
     while(l1 || l2 || remain) {
-      let nodeResult = (l1 ? l1.val : 0) + (l2 ? l2.val : 0)
+      let nodeResult = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + remain
       nodeResult >= 10 ? remain = 1 : remain = 0
       l1 = l1.next || 0
       l2 = l2.next || 0
-      resLinkedList.push(nodeResult % 10 + remain)
+      resLinkedList.push(nodeResult % 10)
     }
     return resLinkedList
 }
