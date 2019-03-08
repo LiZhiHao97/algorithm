bbf26f107f58461cf120e1c86b3d232479f96cc5
lizhihao97 <940166841@qq.com>
2019-03-08 21:29:10 +0800

    update 054

--- leetcode/054_Spiral_Matrix/solution.cpp
+++ leetcode/054_Spiral_Matrix/solution.cpp
@@ -4,21 +4,25 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> v;
-        int m = matrix.size(); // rows
-        if (m == 0) return v;
-        int n = matrix[0] .size(); // cols
-        int i(0), j(0), step(0), remain(m*n);
-        while (remain > 0) {
-            if (remain == 1) { v.push_back(matrix[m/2][n/2]); break; }
-            for (int k = 0; remain > 0 && k < n - 2 * step - 1; ++k, remain--) v.push_back(matrix[i][j++]); // right
-            for (int k = 0; remain > 0 && k < m - 2 * step - 1; ++k, remain--) v.push_back(matrix[i++][j]); // down
-            for (int k = 0; remain > 0 && k < n - 2 * step - 1; ++k, remain--) v.push_back(matrix[i][j--]); // left
-            for (int k = 0; remain > 0 && k < m - 2 * step - 1; ++k, remain--) v.push_back(matrix[i--][j]); // up
-            i++; j++; step++;
-        }
-        return v;
-    }
+  public:
+      vector<int> spiralOrder(vector< vector<int> >& matrix) {
+          vector<int> v;
+          int m = matrix.size(); // rows
+          if (m == 0) return v;
+          int n = matrix[0] .size(); // cols
+          int i(0), j(0), step(0), remain(m*n);
+          while (remain > 0) {
+              if (remain == 1) { v.push_back(matrix[m/2][n/2]); break; }
+              for (int k = 0; remain > 0 && k < n - 2 * step - 1; ++k, remain--) v.push_back(matrix[i][j++]); // right
+              for (int k = 0; remain > 0 && k < m - 2 * step - 1; ++k, remain--) v.push_back(matrix[i++][j]); // down
+              for (int k = 0; remain > 0 && k < n - 2 * step - 1; ++k, remain--) v.push_back(matrix[i][j--]); // left
+              for (int k = 0; remain > 0 && k < m - 2 * step - 1; ++k, remain--) v.push_back(matrix[i--][j]); // up
+              i++; j++; step++;
+          }
+          return v;
+      }
 };
+
+int main() {
+  return 0;
+}
