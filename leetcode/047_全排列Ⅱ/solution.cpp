b882e0fa0899be17d92b7de7682d59355a22bc01
lizhihao97 <940166841@qq.com>
2021-04-21 16:15:20 +0800

    update 047

---
+++ leetcode/047_全排列Ⅱ/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> factorial(n);
+        factorial[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        --k;
+        string ans;
+        vector<int> valid(n + 1, 1);
+        for (int i = 1; i <= n; ++i) {
+            int order = k / factorial[n - i] + 1;
+            for (int j = 1; j <= n; ++j) {
+                order -= valid[j];
+                if (!order) {
+                    ans += (j + '0');
+                    valid[j] = 0;
+                    break;
+                }
+            }
+            k %= factorial[n - i];
+        }
+        return ans;
+    }
+};
