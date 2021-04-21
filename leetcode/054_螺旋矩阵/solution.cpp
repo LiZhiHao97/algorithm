55cf2414ed4ee6869bc15d6abee8c77e3c73d35f
lizhihao97 <940166841@qq.com>
2021-04-21 11:41:33 +0800

    update 054

--- leetcode/054_螺旋矩阵/solution.cpp
+++ leetcode/054_螺旋矩阵/solution.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
-  public:
-      vector<int> spiralOrder(vector< vector<int> >& matrix) {
-          vector<int> v;
-          int m = matrix.size(); // rows
-          if (m == 0) return v;
-          int n = matrix[0] .size(); // cols
-          int i(0), j(0), step(0), remain(m*n);
-          while (remain > 0) {
-              if (remain == 1) { v.push_back(matrix[m/2][n/2]); break; }
-              for (int k = 0; remain > 0 && k < n - 2 * step - 1; ++k, remain--) v.push_back(matrix[i][j++]); // right
-              for (int k = 0; remain > 0 && k < m - 2 * step - 1; ++k, remain--) v.push_back(matrix[i++][j]); // down
-              for (int k = 0; remain > 0 && k < n - 2 * step - 1; ++k, remain--) v.push_back(matrix[i][j--]); // left
-              for (int k = 0; remain > 0 && k < m - 2 * step - 1; ++k, remain--) v.push_back(matrix[i--][j]); // up
-              i++; j++; step++;
-          }
-          return v;
-      }
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int remain = m * n;
+        vector<int> ans;
+        int i = 0, j = 0, step = 0;
+        while (remain) {
+            if (remain == 1) {
+                ans.push_back(matrix[n/2][m/2]);
+                break;
+            }
+            for (; j < m - step - 1 && remain > 0; j++, remain--) ans.push_back(matrix[i][j]);
+            for (; i < n - step - 1 && remain > 0; i++, remain--) ans.push_back(matrix[i][j]);
+            for (; j > step && remain > 0; j--, remain--) ans.push_back(matrix[i][j]);
+            for (; i > step && remain > 0; i--, remain--) ans.push_back(matrix[i][j]);
+            i++;
+            j++;
+            step++;
+        }
+        return ans;
+    }
 };
 
 int main() {
-  return 0;
+    Solution solution;
+    vector<vector<int>> matrix{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> matrix2 = {{1, 2, 3, 4},{5, 6, 7, 8},{9, 10, 11, 12}};
+    vector<int> ans = solution.spiralOrder(matrix2);
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << endl;
+    }
+    return 0;
 }
