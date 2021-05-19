5f92fdcc1c6c38fb6663fb25831f62ca18fb8b70
lizhihao97 <940166841@qq.com>
2021-05-19 16:48:04 +0800

    update 0026

--- leetcode/0026_删除有序数组中的重复项/solution.cpp
+++ leetcode/0026_删除有序数组中的重复项/solution.cpp
@@ -14,7 +14,7 @@ public:
         int fast = 1, slow = 1;
 
         while (fast < n) {
-            if (nums[fast] != nums[fast - 1]) {
+            if (nums[fast] != nums[slow - 1]) {
                 nums[slow] = nums[fast];
                 slow++;
             }
